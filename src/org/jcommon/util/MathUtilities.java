013e2034b59077f831b01f463adf603dcccf1698
reformergirl <reformergirl@gmail.com>
2008-08-27 23:03:17 +0000

    Minor fixes to MathUtilities - removed reference to NumberFormat, corrected precision, and minor StringBuilder fix.

--- src/org/jcommon/util/MathUtilities.java
+++ src/org/jcommon/util/MathUtilities.java
@@ -1,23 +1,17 @@
 package org.jcommon.util;
 
-import java.text.NumberFormat;
-
 public class MathUtilities {
 	public static final float fix(float f, int precision) {
 		float remainder = f % 1.0f;
-		NumberFormat nf = NumberFormat.getInstance(); {
-			nf.setMaximumFractionDigits(30);
-		}
 		
 		// Create min and max
 		StringBuilder minBuilder = new StringBuilder("0.");
 		StringBuilder maxBuilder = new StringBuilder("0.");
-		for (int i = 0; i < precision; i++) {
+		for (int i = 0; i < precision - 1; i++) {
 			minBuilder.append('0');
 			maxBuilder.append('9');
 		}
 		minBuilder.append('1');
-		maxBuilder.append('1');
 		
 		float min = Float.parseFloat(minBuilder.toString());
 		float max = Float.parseFloat(maxBuilder.toString());
@@ -28,10 +22,5 @@ public class MathUtilities {
 			return f + (1.0f - remainder);
 		}
 		return f;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		float f = 5.0000005f;
-		System.out.println("Float: " + f + " - " + fix(f, 5));;
 	}
 }
