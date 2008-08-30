c9ad6f97f3c4b32edd13d5a2c18c09edb0a3fa21
darkfrog26 <darkfrog26@gmail.com>
2008-08-30 23:23:58 +0000

    Added ImageUtils to support creation of thumbnails of pre-existing images.

---
+++ src/org/jcommon/image/ImageUtils.java
@@ -0,0 +1,64 @@
+package org.jcommon.image;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageUtils {
+	/**
+	 * Takes an input image file and writes an output image with the dimensions specified in width and height.
+	 * 
+	 * @param input
+	 * @param output
+	 * @param size
+	 * @throws IOException
+	 */
+	public static final void createThumbnail(File input, File output, int size) throws IOException {
+		// Read the source image
+		Image source = ImageIO.read(input);
+		
+		// Create a target image from the source using width
+		int width = -1;
+		int height = -1;
+		if (source.getWidth(null) > source.getHeight(null)) {
+			width = size;
+		} else {
+			height = size;
+		}
+		Image target = source.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		if (target.getHeight(null) > height) {
+			// The height is larger than it should be, so we switch to height-based scaling
+			target = source.getScaledInstance(-1, height, Image.SCALE_SMOOTH);
+		}
+		
+		// Draw target to a RenderedImage
+		BufferedImage bi = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB); {
+			Graphics g = bi.getGraphics(); {
+				// Draw a white background
+				g.setColor(Color.WHITE);
+				g.fillRect(0, 0, size, size);
+				
+				// Determine location to draw at
+				int x = (size - target.getWidth(null)) / 2;
+				int y = (size - target.getHeight(null)) / 2;
+				
+				// Draw image to canvas
+				g.drawImage(target, x, y, null);
+			}
+		}
+		
+		// Output the image to the output file
+		ImageIO.write(bi, "jpeg", output);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		long time = System.currentTimeMillis();
+		createThumbnail(new File("temp.jpg"), new File("temp_thumb.jpg"), 75);
+		System.out.println("Time: " + (System.currentTimeMillis() - time));
+	}
+}
